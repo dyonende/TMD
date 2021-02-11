9109de02bd60f7452ce8d6ed854e50e2d4e6d6bd
Dyon <dyonx@hotmail.com>
2021-02-11 16:10:22 +0100

    pdf download script

---
+++ code/pdf_downloader.py
@@ -0,0 +1,57 @@
+'''
+@authors: Dyon van der Ende, Eva den Uijl, Myrthe Buckens
+download pdf files of articles
+'''
+import pandas as pd
+import requests
+import argparse
+import sys
+
+
+def collect_links(input_file):
+    '''
+    collect the id and url to article for each entry in the data
+    '''
+    with open(input_file) as infile:
+        df = pd.read_csv(infile, delimiter=',')
+        ids = df['_id']
+        links = df['unpaywall_response.free_fulltext_url'] #column with urls
+        if len(links)==len(ids):
+            return ids, links
+            
+def download_pdf(ids, links, output_path):
+    '''
+    try to download the pdf at the specified url and saved 
+    it to a file with the id as name
+    '''
+    number_of_links = len(links)
+    for i in range(number_of_links):
+        print(str(i/number_of_links*100)[:4]+"%", end="\r") #print progress
+        id = ids[i]
+        link = links[i]
+        filename = id+'.pdf'
+        try:            
+            r = requests.get(link, stream=True) 
+            with open(output_path+filename, 'wb') as outfile:
+                outfile.write(r.content)
+        except KeyboardInterrupt:
+            sys.exit()
+        except:
+            print(id +" failed to download")
+          
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file',
+                        help='file path to data')
+    parser.add_argument('output_path',
+                        help='file path to store pdf\'s')
+    args = parser.parse_args()
+    input_file = args.input_file
+    output_path = args.output_path
+
+    ids, links = collect_links(input_file)
+    download_pdf(ids, links, output_path)
+
+if __name__ == '__main__':
+    main()
